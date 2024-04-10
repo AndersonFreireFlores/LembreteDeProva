7cd8541854e2247e7bee173485e2b1d8838716a1
Anderson Freire Flores <andersonfflores75@gmail.com>
2024-04-10 15:28:07 -0300

    Initial commit

---
+++ src/main/java/andersonfflores/lembretedeprova/LembreteDeProvaApplication.java
@@ -0,0 +1,13 @@
+package andersonfflores.lembretedeprova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LembreteDeProvaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LembreteDeProvaApplication.class, args);
+    }
+
+}
