86d004e8fad700d2d2de5c61f686a251c15bdeaa
Anderson Freire Flores <andersonfflores75@gmail.com>
2024-04-11 12:59:24 -0300

    Lembrete entity

---
+++ src/main/java/andersonfflores/lembretedeprova/entities/Lembrete.java
@@ -0,0 +1,39 @@
+package andersonfflores.lembretedeprova.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Lembrete {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    UUID id;
+
+    String Materia;
+
+    String professor;
+
+    String descrição;
+
+    @ManyToOne
+    @JoinColumn(name = "criador_id")
+    User criador;
+
+    LocalDateTime creationDate;
+
+    LocalDate lembreteDate;
+
+
+}
