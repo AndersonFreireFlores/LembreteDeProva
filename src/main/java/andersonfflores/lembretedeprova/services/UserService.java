3ab1bf47bebba5089eabafe140f9d8db826badb7
Anderson Freire Flores <andersonfflores75@gmail.com>
2024-04-11 13:29:41 -0300

    User Controller

---
+++ src/main/java/andersonfflores/lembretedeprova/services/UserService.java
@@ -0,0 +1,47 @@
+package andersonfflores.lembretedeprova.services;
+
+import andersonfflores.lembretedeprova.entities.User;
+import andersonfflores.lembretedeprova.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository repository;
+
+    public Optional<User> findById(UUID id){
+        return repository.findById(id);
+    }
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    public User createUser(User user){
+        return repository.save(user);
+    }
+
+    public User updateUser(UUID id, User user){
+        Optional<User> userOptional = repository.findById(id);
+        if(userOptional.isPresent()){
+            userOptional.get().setName(user.getName());
+            userOptional.get().setEmail(user.getEmail());
+        }
+        return null;
+    }
+
+    public void deleteByID(UUID id){
+        repository.deleteById(id);
+    }
+
+    public void deleteAll(){
+        repository.deleteAll();
+    }
+
+}
